0136621b44c551783f83353103b115c479b05e5d
diegoAME <44954181+diegoAME@users.noreply.github.com>
2019-04-29 13:06:31 -0500

    Add files via upload

---
+++ souvenir/souvenir/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  souvenir
+//
+//  Created by MacBook on 4/29/19.
+//  Copyright © 2019 UNAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var shirtStepperOutlet: UIStepper!
+    
+    @IBOutlet weak var shirtNumberField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+    
+
+
+    
+    
+}
+
