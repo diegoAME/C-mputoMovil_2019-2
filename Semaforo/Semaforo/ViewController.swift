7af6c151a732f396b93101a8d01e94dd5c648635
diegoAME <44954181+diegoAME@users.noreply.github.com>
2020-01-07 12:38:47 -0600

    Add files via upload

---
+++ Semaforo/Semaforo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Semaforo
+//
+//  Created by 2020-2 on 07/01/20.
+//  Copyright © 2020 2020-2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func unwindToGreen(unwindSegue: UIStoryboardSegue){
+        
+    }
+
+}
+
